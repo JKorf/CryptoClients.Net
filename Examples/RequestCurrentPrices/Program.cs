a34a8b0541d5119df0c3482c3e8df17681612f36
Jkorf <jankorf91@gmail.com>
2025-05-14 13:38:14 +0200

    Updated examples

--- Examples/RequestCurrentPrices/Program.cs
+++ Examples/RequestCurrentPrices/Program.cs
@@ -20,7 +20,7 @@ foreach (var symbol in symbols)
     var sw = Stopwatch.StartNew();
     var tickers = await client.GetSpotTickerAsync(new GetTickerRequest(symbol));
     foreach (var result in tickers)
-        Console.WriteLine($"{result.Exchange}: {result.Data.LastPrice}, result in {sw.ElapsedMilliseconds}ms");
+        Console.WriteLine($"{result.Exchange}: {result.Data?.LastPrice}, result in {sw.ElapsedMilliseconds}ms");
     sw.Stop();
     Console.WriteLine();
     Console.WriteLine($"Method 1, All exchanges completed in {sw.ElapsedMilliseconds}ms");
@@ -29,7 +29,7 @@ foreach (var symbol in symbols)
     // Method 2, GetSpotTickerAsyncEnumerable will return results whenever a request is finished instead of waiting for all requests
     sw.Restart();
     await foreach (var result in client.GetSpotTickerAsyncEnumerable(new GetTickerRequest(symbol)))
-        Console.WriteLine($"{result.Exchange}: {result.Data.LastPrice}, result in {sw.ElapsedMilliseconds}ms");
+        Console.WriteLine($"{result.Exchange}: {result.Data?.LastPrice}, result in {sw.ElapsedMilliseconds}ms");
     sw.Stop();
 
     Console.WriteLine();
